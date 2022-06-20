376bb179ebf1f17c389ea739ff39bbfec94d90ad
Andrei Knyshev <asknyshev@gmail.com>
2022-06-20 03:42:17 +0300

    update Notes component to display nothing if everything is filtered

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,16 +2,16 @@ import PropTypes from 'prop-types';
 import Note from './Note';
 
 const Notes = ({ notes, togglePinned, searchFilter }) => {
+  const filteredNotes = notes
+    .filter((note) => note.content.toLowerCase().includes(searchFilter.trim().toLowerCase()))
+    .map((note) => <Note key={note.id} note={note} togglePinned={togglePinned} />);
+
   return (
     <div>
-      <h2 className="notes-header">{notes[0].pinned ? 'Pinned' : 'Others'}</h2>
-      <div className="notes">
-        {notes
-          .filter((note) => note.content.toLowerCase().includes(searchFilter.trim().toLowerCase()))
-          .map((note) => (
-            <Note key={note.id} note={note} togglePinned={togglePinned} />
-          ))}
-      </div>
+      {filteredNotes.length > 0 && (
+        <h2 className="notes-header">{notes[0].pinned ? 'Pinned' : 'Others'}</h2>
+      )}
+      {filteredNotes.length > 0 && <div className="notes">{filteredNotes}</div>}
     </div>
   );
 };
