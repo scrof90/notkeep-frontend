ccc94dc02b0e4f2158f356c2c9914c553559ad9d
Andrei Knyshev <asknyshev@gmail.com>
2022-06-21 03:28:52 +0300

    exctract Notification styles into a module

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
+import styles from './Notification.module.css';
 
 const Notification = ({ message }) => {
   if (message === null) {
     return null;
   }
 
-  return <div className="error">{message}</div>;
+  return <div className={styles.error}>{message}</div>;
 };
 
 Notification.propTypes = {
