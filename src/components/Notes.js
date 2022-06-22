772b85ec0d913ec8ba918db358252b3d803a42d5
Andrei Knyshev <asknyshev@gmail.com>
2022-06-22 05:49:51 +0300

    update styles for different layouts in Notes component

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -5,16 +5,18 @@ import styles from './Notes.module.css';
 const Notes = ({ notes, togglePinned, isListView }) => {
   const notesPinned = notes.filter((note) => note.pinned);
   const notesUnpinned = notes.filter((note) => !note.pinned);
+  const wrapperLayout = isListView ? `${styles.wrapper} ${styles.centered}` : styles.wrapper;
+  const notesLayout = isListView ? styles.listLayout : styles.gridLayout;
 
   return (
-    <div className={styles.wrapper}>
+    <div className={wrapperLayout}>
       {notesPinned.length && <h2 className={styles.header}>Pinned</h2>}
-      <div className={isListView ? styles.listView : styles.gridView}>
+      <div className={notesLayout}>
         {notesPinned.length > 0 &&
           notesPinned.map((note) => <Note key={note.id} note={note} togglePinned={togglePinned} />)}
       </div>
       {notesUnpinned.length && <h2 className={styles.header}>Others</h2>}
-      <div className={isListView ? styles.listView : styles.gridView}>
+      <div className={notesLayout}>
         {notesUnpinned.length &&
           notesUnpinned.map((note) => (
             <Note key={note.id} note={note} togglePinned={togglePinned} />
