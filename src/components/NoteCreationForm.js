934888a601a5b4335d77bb6afed89ce813c9a26e
Andrei Knyshev <asknyshev@gmail.com>
2022-06-27 05:09:10 +0300

    add OutsideClickHandler

--- src/components/NoteCreationForm.js
+++ src/components/NoteCreationForm.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import OutsideClickHandler from 'react-outside-click-handler';
 import { MdPushPin, MdOutlinePushPin } from 'react-icons/md';
 import styles from './NoteCreationForm.module.css';
 
@@ -13,31 +14,33 @@ const NoteCreationForm = ({
   onPinnedChange
 }) => {
   const [isBlurred, setIsBlurred] = useState(true);
-  const handleOnFocusChange = () => setIsBlurred(!isBlurred);
+  const handleFocus = () => setIsBlurred(false);
+  const handleBlur = () => setIsBlurred(true);
 
   return (
-    <form
-      onFocus={handleOnFocusChange}
-      onBlur={handleOnFocusChange}
-      className={`${styles.wrapper} ${isBlurred && styles.blurred}`}
-      onSubmit={onSubmit}
-    >
-      {!isBlurred && (
-        <button className={styles.pin} value={pinnedValue} onChange={onPinnedChange}>
-          {pinnedValue ? <MdPushPin /> : <MdOutlinePushPin />}
-        </button>
-      )}
-      {!isBlurred && (
-        <input type="textarea" value={titleValue} onChange={onTitleChange} placeholder="Title" />
-      )}
-      <input
-        type="textarea"
-        value={contentValue}
-        onChange={onContentChange}
-        placeholder="Take a note..."
-      />
-      {!isBlurred && <button type="submit">save</button>}
-    </form>
+    <OutsideClickHandler onOutsideClick={handleBlur}>
+      <form
+        onFocus={handleFocus}
+        className={`${styles.wrapper} ${isBlurred && styles.blurred}`}
+        onSubmit={onSubmit}
+      >
+        {!isBlurred && (
+          <button className={styles.pin} value={pinnedValue} onChange={onPinnedChange}>
+            {pinnedValue ? <MdPushPin /> : <MdOutlinePushPin />}
+          </button>
+        )}
+        {!isBlurred && (
+          <input type="textarea" value={titleValue} onChange={onTitleChange} placeholder="Title" />
+        )}
+        <input
+          type="textarea"
+          value={contentValue}
+          onChange={onContentChange}
+          placeholder="Take a note..."
+        />
+        {!isBlurred && <button type="submit">save</button>}
+      </form>
+    </OutsideClickHandler>
   );
 };
 
