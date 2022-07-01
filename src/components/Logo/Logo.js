25a95cd5a9f3ef5f91ce0b6521a8a45470c7297f
Andrei Knyshev <asknyshev@gmail.com>
2022-07-02 02:48:02 +0300

    update Logo styles

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,8 +3,8 @@ import classes from './styles.module.scss';
 
 const Logo = () => {
   return (
-    <div className={classes.wrapper}>
-      <div>
+    <div className={classes.logo}>
+      <div className={classes.iconContainer}>
         <MdInsertDriveFile />
       </div>
       <h1>NotKeep</h1>
