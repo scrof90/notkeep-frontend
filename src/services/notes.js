60bcc4b25489f85f8244308aa5cb851195f24bc0
Andrei Knyshev <asknyshev@gmail.com>
2022-07-20 03:01:17 +0300

    refactor notes service to async/await syntax

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,28 +1,25 @@
 import axios from 'axios';
 // URL for production build
-// const baseUrl = '/api/notes';
+const baseUrl = '/api/notes';
 // URL for development via json-server
-const baseUrl = 'http://localhost:3003/api/notes';
+// const baseUrl = 'http://localhost:3003/api/notes';
 
-const getAll = () => {
-  const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
+const getAll = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
 };
 
-const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject);
-  return request.then((response) => response.data);
+const create = async (newObject) => {
+  const response = await axios.post(baseUrl, newObject);
+  return response.data;
 };
 
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then((response) => response.data);
+const update = async (id, newObject) => {
+  const response = await axios.put(`${baseUrl}/${id}`, newObject);
+  return response.data;
 };
 
-const remove = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`);
-  return request.then((response) => response);
-};
+const remove = async (id) => await axios.delete(`${baseUrl}/${id}`);
 
 export default {
   getAll,
