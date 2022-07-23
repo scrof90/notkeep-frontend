d17522b2d53c85683f308bd9ea5d64d38bd3d420
Andrei Knyshev <asknyshev@gmail.com>
2022-07-23 03:34:38 +0300

    add tooltipText prop

--- src/components/ui/BtnWithIconLarge.js
+++ src/components/ui/BtnWithIconLarge.js
@@ -1,17 +1,20 @@
 import PropTypes from 'prop-types';
+import BtnTooltip from './BtnTooltip';
 import classes from './assets/BtnWithIconLarge.module.scss';
 
-const BtnWithIconLarge = ({ onClick, icon: Icon }) => {
+const BtnWithIconLarge = ({ onClick, icon: Icon, tooltipText }) => {
   return (
     <button className={classes.btnWithIconLarge} onClick={onClick} type="button">
       <Icon />
+      <BtnTooltip tip={tooltipText} />
     </button>
   );
 };
 
 BtnWithIconLarge.propTypes = {
   onClick: PropTypes.func.isRequired,
-  icon: PropTypes.func.isRequired
+  icon: PropTypes.func.isRequired,
+  tooltipText: PropTypes.string.isRequired
 };
 
 export default BtnWithIconLarge;
